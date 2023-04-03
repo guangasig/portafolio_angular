3c22c6fb8a735e0ca631d45b1715cef0bf0f37c3
guangasig <guangasigtsm@gmail.com>
2023-04-03 00:27:58 -0500

    Animación básica con gsap

--- src/app/modules/portafolio/portafolio-contact/portafolio-contact.component.ts
+++ src/app/modules/portafolio/portafolio-contact/portafolio-contact.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { faPhone, faEnvelope, faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { TimelineLite, TimelineMax, Back, Power1, gsap } from 'gsap'
 
 @Component({
   selector: 'app-portafolio-contact',
   templateUrl: './portafolio-contact.component.html',
   styleUrls: ['./portafolio-contact.component.css']
 })
-export class PortafolioContactComponent {
+export class PortafolioContactComponent implements AfterViewInit {
   
   public faPhone= faPhone;
   public faEnvelope= faEnvelope;
   public faMapLocationDot= faMapLocationDot;
+
+  @ViewChild('box1') box: ElementRef;
+
+  ngAfterViewInit(){
+    gsap.fromTo(this.box.nativeElement, 
+      {duration:2, ease: "power2.out", x: -100}, 
+      {duration:2, ease: "power2.out", x: 0}
+    );
+  }
 
 }
